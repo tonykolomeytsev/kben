46d1c599f75d575c0015b49c62f53ba71659ab3a
a.kolomeytsev <akolomeytsev@vivid.money>
2021-12-14 21:50:34 +0300

    Improve TypeHolder

--- kben-core/src/main/kotlin/kekmech/kben/TypeHolder.kt
+++ kben-core/src/main/kotlin/kekmech/kben/TypeHolder.kt
@@ -1,41 +1,84 @@
 package kekmech.kben
 
 import java.lang.reflect.ParameterizedType
+import java.lang.reflect.Type
+import java.lang.reflect.WildcardType
 import kotlin.reflect.KClass
 import kotlin.reflect.KParameter
 import kotlin.reflect.jvm.javaType
 
 sealed class TypeHolder {
 
-    data class Simple(val type: KClass<*>) : TypeHolder()
+    data class Simple(val type: KClass<*>) : TypeHolder() {
 
-    data class Parameterized(val type: KClass<*>, val parameterTypes: List<KClass<*>>) : TypeHolder()
+        override fun toString(): String = "*${type.simpleName}"
+    }
+
+    data class Parameterized(val type: KClass<*>, val parameterTypes: List<TypeHolder>) : TypeHolder() {
+
+        override fun toString(): String = "*${type.simpleName}${
+            parameterTypes.joinToString(", ", "<", ">")
+        }"
+    }
 
     companion object {
 
         fun ofBencodeList(valueKClass: KClass<*>): TypeHolder =
             Parameterized(
                 type = Iterable::class,
-                parameterTypes = listOf(valueKClass)
+                parameterTypes = listOf(Simple(valueKClass))
             )
 
         fun ofBencodeDictionary(valueKClass: KClass<*>): TypeHolder =
             Parameterized(
                 type = Map::class,
-                parameterTypes = listOf(String::class, valueKClass)
+                parameterTypes = listOf(Simple(String::class), Simple(valueKClass))
             )
 
-        fun from(parameter: KParameter): TypeHolder =
-            when (val type = parameter.type.javaType) {
+        fun from(parameter: KParameter): TypeHolder {
+            val type = parameter.type.javaType
+            return type.parameterTypes2()
+        }
+
+        fun inner(typeHolder: Parameterized): TypeHolder {
+            val typeParametersCount = typeHolder.type.typeParameters.size
+            return Parameterized(
+                type = typeHolder.parameterTypes[typeParametersCount].type,
+                parameterTypes = typeHolder.parameterTypes.drop(typeParametersCount)
+            )
+        }
+
+
+        private fun Type.parameterTypes(): List<Type> {
+            return when (this) {
+                is Class<*> -> listOf(this)
+                is ParameterizedType -> listOf(rawType) + actualTypeArguments.flatMap { it.parameterTypes() }
+                is WildcardType -> upperBounds.flatMap { it.parameterTypes() }
+                else -> TODO("Not implemented")
+            }
+        }
+
+        private fun Type.parameterTypes2(): TypeHolder {
+            return when (this) {
                 is ParameterizedType ->
                     Parameterized(
-                        type = (type.rawType as Class<*>).kotlin,
-                        parameterTypes = type.actualTypeArguments.map { (it as Class<*>).kotlin },
+                        type = (rawType as Class<*>).kotlin,
+                        parameterTypes = actualTypeArguments.map { it.parameterTypes2() }
                     )
+                is WildcardType -> upperBounds[0].parameterTypes2()
                 else ->
-                    Simple(
-                        type = (type as Class<*>).kotlin
-                    )
+                    Simple(type = (this as? Class<*>)?.kotlin ?: throw cantInferTypeError(this))
             }
+        }
+
+        private fun cantInferTypeError(type: Type): Throwable =
+            IllegalStateException("Can't infer type $type")
+
+        private val TypeHolder.type
+            get() =
+                when (this) {
+                    is Simple -> type
+                    is Parameterized -> type
+                }
     }
 }
--- kben-core/src/test/kotlin/kekmech/kben/TypeHolderTest.kt
+++ kben-core/src/test/kotlin/kekmech/kben/TypeHolderTest.kt
@@ -5,9 +5,15 @@ import kotlin.reflect.full.primaryConstructor
 
 class TypeHolderTest {
 
-    data class TestClass<T>(
-        val id: T,
-        val a: List<List<String>>,
+    data class Id<A, B, C>(
+        val a: A,
+        val b: B,
+        val c: C,
+    )
+
+    data class TestClass(
+        val id: Id<List<Long>, Int, String>,
+        val b: List<Map<String, List<Long>>>,
     )
 
     @Test
