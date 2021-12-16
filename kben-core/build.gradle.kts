091a22ddf3ba174722aaf082a36335e89a3f621b
Anton Kolomeytsev <tonykolomeytsev@gmail.com>
2021-12-17 00:57:37 +0300

    Add maven publish plugin to projects, fix CI script

--- kben-core/build.gradle.kts
+++ kben-core/build.gradle.kts
@@ -1,8 +1,23 @@
 plugins {
     kotlin("jvm")
+    `maven-publish`
 }
 
 dependencies {
     testImplementation(kotlin("test"))
     implementation(kotlin("reflect"))
+}
+
+afterEvaluate {
+    publishing {
+        publications {
+            create<MavenPublication>("maven") {
+                groupId = project.group as String
+                artifactId = project.name
+                version = project.version as String
+
+                from(components["java"])
+            }
+        }
+    }
 }
--- kben-retrofit-converter/build.gradle.kts
+++ kben-retrofit-converter/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     kotlin("jvm")
+    `maven-publish`
 }
 
 dependencies {
@@ -7,4 +8,18 @@ dependencies {
     testImplementation("com.squareup.okhttp3:mockwebserver:4.9.3")
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation(project(":kben-core"))
+}
+
+afterEvaluate {
+    publishing {
+        publications {
+            create<MavenPublication>("maven") {
+                groupId = project.group as String
+                artifactId = project.name
+                version = project.version as String
+
+                from(components["java"])
+            }
+        }
+    }
 }
