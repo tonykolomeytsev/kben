8ecbcf030cf31d3d43ef7ce787fa6258c76e4b27
a.kolomeytsev <akolomeytsev@vivid.money>
2021-12-19 12:02:12 +0300

    Add @Bencode and @DefaultValue annotations support for Enums

--- kben-core/src/main/kotlin/kekmech/kben/domain/adapters/EnumTypeAdapter.kt
+++ kben-core/src/main/kotlin/kekmech/kben/domain/adapters/EnumTypeAdapter.kt
@@ -1,20 +1,23 @@
 package kekmech.kben.domain.adapters
 
 import kekmech.kben.TypeHolder
+import kekmech.kben.annotations.Bencode
+import kekmech.kben.annotations.DefaultValue
 import kekmech.kben.domain.DeserializationContext
 import kekmech.kben.domain.SerializationContext
 import kekmech.kben.domain.TypeAdapter
 import kekmech.kben.domain.dto.BencodeElement
 import kekmech.kben.domain.dto.BencodeElement.BencodeByteString
 
-class EnumTypeAdapter<T : Any> : TypeAdapter<Enum<*>>() {
+internal class EnumTypeAdapter : TypeAdapter<Enum<*>>() {
 
     @Suppress("UNCHECKED_CAST")
     override fun fromBencode(value: BencodeElement, context: DeserializationContext, typeHolder: TypeHolder): Enum<*> {
         typeHolder as TypeHolder.Simple // enum classes cannot have type parameters
         val serializedOptionName = (value as BencodeByteString).asString
         val options = typeHolder.type.java.enumConstants as Array<Enum<*>>
-        return options.firstOrNull { it.name == serializedOptionName }
+        return options.firstOrNull { it.annotatedName == serializedOptionName }
+            ?: options.firstOrNull { it.isDefaultOption }
             ?: throw optionError(
                 enumClassName = typeHolder.type.qualifiedName!!,
                 optionName = serializedOptionName,
@@ -22,7 +25,13 @@ class EnumTypeAdapter<T : Any> : TypeAdapter<Enum<*>>() {
     }
 
     override fun toBencode(value: Enum<*>, context: SerializationContext): BencodeElement =
-        BencodeByteString(value.name)
+        BencodeByteString(value.annotatedName)
+
+    private val Enum<*>.annotatedName: String
+        get() = (javaClass.getField(name).annotations.firstOrNull { it is Bencode } as? Bencode)?.name ?: name
+
+    private val Enum<*>.isDefaultOption: Boolean
+        get() = javaClass.getField(name).annotations.any { it is DefaultValue }
 
     private fun optionError(enumClassName: String, optionName: String): Throwable =
         IllegalStateException("Option with name '$optionName' not found in enum class $enumClassName")
